41f71db86a957a798103f81a4fee630491764148
Mischa Dhar <mischadhar94@gmail.com>
2022-09-08 11:55:55 +0100

    pushing

--- scripts/API_package/chartmetric/interface/main.py
+++ scripts/API_package/chartmetric/interface/main.py
@@ -8,21 +8,24 @@ from chartmetric.data_processing.params import ACCESS_TOKEN
 
 
 def setup():
+    """Artist group should equal cancelled or control"""
 
-    df = read_artist_data(base_url = LOCAL_DATA_PATH)
+    df = read_artist_data(base_url = f"{LOCAL_DATA_PATH}")
 
-    reduced_df = df[['ARTIST','CHARTMETRIC ID', 'DATE OF CANCELLATION']]
+    df_reduced = df[['ARTIST','CHARTMETRIC ID', 'DATE OF CANCELLATION','CANCELLED']]
 
-    return reduced_df
+    return df_reduced
 
-def dataframe_pipeline(reduced_df):
+def dataframe_pipeline(df):
+    """Artist group should equal cancelled or control"""
 
-    for artist_id, incident_date, artist in zip(reduced_df['CHARTMETRIC ID']
-                                                , reduced_df['DATE OF CANCELLATION']
-                                                , reduced_df['ARTIST']):
+    for artist_id, incident_date, artist, cancelled in zip(df['CHARTMETRIC ID']
+                                                , df['DATE OF CANCELLATION']
+                                                , df['ARTIST']
+                                                , df['CANCELLED']):
 
         since_date, until_date = date_calculator(incident_date=incident_date
-                                                 ,n_months=6)
+                                                ,n_months=6)
 
         spotify_response = spotify_API_call(since_date = since_date
                                             , until_date= until_date
@@ -36,11 +39,11 @@ def dataframe_pipeline(reduced_df):
         spotify_df = spotify_df_generator(spotify_response)
 
         radio_df = radio_df_generator(radio_response
-                                      , until_date=until_date)
+                                    , until_date=until_date)
 
         merged_df = merge_spotify_radio(spotify_df=spotify_df
                                         , radio_df=radio_df)
 
-        filepath = f"{LOCAL_DATA_PATH}/API_data"
+        filepath = f"{LOCAL_DATA_PATH}/API_data/{cancelled}"
 
         merged_df.to_csv(f"{filepath}/{artist}_incedent_{incident_date}.csv")
