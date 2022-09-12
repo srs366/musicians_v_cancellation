89279206d83eb57062df0a7e1e73e81983ec9ac5
Mischa Dhar <mischadhar94@gmail.com>
2022-09-12 18:00:55 +0100

    updated charts to include instagram data

--- scripts/Merging_Data/Charting/make_charts.py
+++ scripts/Merging_Data/Charting/make_charts.py
@@ -3,19 +3,36 @@ from matplotlib import pyplot as plt
 
 def generate_chart(df, artist, artist_id, base_url, artist_type, date):
 
-    fig, axes = plt.subplots(2, 1, sharex=True, figsize=(20,15))
+    fig, axes = plt.subplots(5, 1, sharex=True, figsize=(30,15))
     fig.suptitle(f'Charts for {artist}: incident date {date}')
 
     axes[0].set_title('Spotify monthly listens vs Tweets')
     sns.lineplot(data=df.monthly_listeners, color="b", ax=axes[0])
     ax2 = axes[0].twinx()
     sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax2)
-    sns.lineplot(data=df.TweetSentiment_Positive, color="g", ax=ax2)
+    # sns.lineplot(data=df.TweetSentiment_Positive, color="g", ax=ax2)
 
     axes[1].set_title('Radio monthly plays vs Tweets')
     sns.lineplot(data=df.monthly_spins, color="b", ax=axes[1])
     ax3 = axes[1].twinx()
     sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax3)
-    sns.lineplot(data=df.TweetSentiment_Positive, color="g", ax=ax3)
+    # sns.lineplot(data=df.TweetSentiment_Positive, color="g", ax=ax3)
+
+    axes[2].set_title('Followers (insta) vs Tweets')
+    sns.lineplot(data=df.followers_insta, color="b", ax=axes[2])
+    ax4 = axes[1].twinx()
+    sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax4)
+
+    axes[3].set_title('Av post likes (insta) vs Tweets')
+    sns.lineplot(data=df.av_post_likes_insta, color="b", ax=axes[3])
+    ax5 = axes[1].twinx()
+    sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax5)
+
+    axes[4].set_title('Av post comments (insta) vs Tweets')
+    sns.lineplot(data=df.av_post_comments_insta, color="b", ax=axes[4])
+    ax6 = axes[1].twinx()
+    sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax6)
 
     fig.savefig(f'{base_url}/Exploratory_charts/{artist_type}/{artist_id}.png')
+
+    plt.close()
--- scripts/Merging_Data/Preprocessing/chartmetric_clean.py
+++ scripts/Merging_Data/Preprocessing/chartmetric_clean.py
@@ -9,14 +9,9 @@ def read_chartmetric_data(base_url, artist_id, incident_date, artist_type):
 
 def clean_chartmetric_data(df):
 
-    chartmetric_trim = df.drop(columns=['Unnamed: 0','listener_change',
-       'spins_change', 'followers_insta', 'av_post_likes_insta',
-       'av_post_comments_insta', 'av_post_views_insta',
-       'follower_pct_change_insta', 'followers_tiktok', 'av_post_likes_tiktok',
-       'av_post_comments_tiktok', 'av_post_views_tiktok',
-       'follower_pct_change_tiktok', 'followers_youtube',
-       'av_post_likes_youtube', 'av_post_comments_youtube',
-       'av_post_views_youtube', 'follower_pct_change_youtube'])
+    chartmetric_trim = df.drop(columns=['Unnamed: 0','listener_change'
+                                        ,'spins_change', 'follower_pct_change_insta'
+                                        ,'follower_pct_change_tiktok', 'follower_pct_change_youtube'])
 
     chartmetric_trim['date'] = pd.to_datetime(chartmetric_trim['date'])
 
