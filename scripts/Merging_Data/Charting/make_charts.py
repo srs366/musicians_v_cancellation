92ec43d2f8f88207142f55acb58621032b2dabd6
Mischa Dhar <mischadhar94@gmail.com>
2022-09-13 12:11:38 +0100

    updated chart scripts

--- scripts/Merging_Data/Charting/make_charts.py
+++ scripts/Merging_Data/Charting/make_charts.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 
 def generate_chart(df, artist, artist_id, base_url, artist_type, date):
 
-    fig, axes = plt.subplots(5, 1, sharex=True, figsize=(30,15))
+    fig, axes = plt.subplots(2, 1, sharex=True, figsize=(20,15))
     fig.suptitle(f'Charts for {artist}: incident date {date}')
 
     axes[0].set_title('Spotify monthly listens vs Tweets')
@@ -18,20 +18,20 @@ def generate_chart(df, artist, artist_id, base_url, artist_type, date):
     sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax3)
     # sns.lineplot(data=df.TweetSentiment_Positive, color="g", ax=ax3)
 
-    axes[2].set_title('Followers (insta) vs Tweets')
-    sns.lineplot(data=df.followers_insta, color="b", ax=axes[2])
-    ax4 = axes[1].twinx()
-    sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax4)
+    # axes[2].set_title('Av post views (insta)')
+    # sns.lineplot(data=df.av_post_views_insta, color="b", ax=axes[2])
+    # # ax4 = axes[1].twinx()
+    # # sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax4)
 
-    axes[3].set_title('Av post likes (insta) vs Tweets')
-    sns.lineplot(data=df.av_post_likes_insta, color="b", ax=axes[3])
-    ax5 = axes[1].twinx()
-    sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax5)
+    # axes[3].set_title('Av post views (tiktok)')
+    # sns.lineplot(data=df.av_post_views_tiktok, color="b", ax=axes[3])
+    # # ax5 = axes[1].twinx()
+    # # sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax5)
 
-    axes[4].set_title('Av post comments (insta) vs Tweets')
-    sns.lineplot(data=df.av_post_comments_insta, color="b", ax=axes[4])
-    ax6 = axes[1].twinx()
-    sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax6)
+    # axes[4].set_title('Av post views (youtube)')
+    # sns.lineplot(data=df.av_post_views_youtube, color="b", ax=axes[4])
+    # # ax6 = axes[1].twinx()
+    # # sns.lineplot(data=df.TweetSentiment_Negative, color="r", ax=ax6)
 
     fig.savefig(f'{base_url}/Exploratory_charts/{artist_type}/{artist_id}.png')
 
