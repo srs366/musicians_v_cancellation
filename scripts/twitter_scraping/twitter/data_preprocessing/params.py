e2cb4641f2de867972a04682bc34925b200dc9e8
Afiq Hamidon <malaiafiq@gmail.com>
2022-09-14 16:06:21 +0100

    pushing things

--- scripts/twitter_scraping/twitter/data_preprocessing/params.py
+++ scripts/twitter_scraping/twitter/data_preprocessing/params.py
@@ -1,3 +1,3 @@
 import os
 
-LOCAL_DATA_PATH = os.path.expanduser(os.environ.get('LOCAL_DATA_PATH'))
+LOCAL_DATA_PATH = os.path.expanduser(os.environ.get('musicians_v_cancellation/raw_data/artist_data.csv'))
